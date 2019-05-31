bd7d7bc9eb52e1bbfbf0bce2567eff7aae1f6fdb
KeijiTagami <43602774+KeijiTagami@users.noreply.github.com>
2019-05-31 15:43:08 +0900

    Update Template.c

--- Template.c
+++ Template.c
@@ -4,8 +4,9 @@
 int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	DxLib_Init();	// DXライブラリ初期化処理
 	ChangeWindowMode(TRUE);
-	LoadGraphScreen(0, 0, "Graphic/testdot.png", TRUE);//これを書く
+	DrawBox(60, 120, 480, 240, GetColor(128, 64, 64), TRUE);
 	WaitKey();	// キー入力待ち
+	
 	DxLib_End();	// DXライブラリ終了処理
 	return 0;
 	/* 入力が終わった人は以下を
