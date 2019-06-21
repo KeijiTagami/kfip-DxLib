40074e1cb146eecbb63076c2d4a2f0bd953326d2
KeijiTagami <43602774+KeijiTagami@users.noreply.github.com>
2019-06-21 15:46:21 +0900

    Update Template.c

--- Template.c
+++ Template.c
@@ -7,9 +7,4 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	WaitKey();	// キー入力待ち
 	DxLib_End();	// DXライブラリ終了処理
 	return 0;
-	/* 入力が終わった人は以下を
-	WaitKey();の一つ前に入力してみる。
-
-	DrawBox(60,120,480,240,GetColor(128,64,64),TRUE);
-	*/
 }
